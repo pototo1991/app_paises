a958b65e7d24c1470ac8993c02156034adf24ff1
walter <whsg27@gmail.com>
2024-11-05 23:07:57 -0300

    nueva actualizacion

--- app_paises/settings.py
+++ app_paises/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv(
 DEBUG = os.getenv("DEBUG", "False") == "False"
 
 # Hosts permitidos
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "app_paises.onrender.com").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "app-paises.onrender.com").split(",")
 
 
 # Aplicaciones instaladas
